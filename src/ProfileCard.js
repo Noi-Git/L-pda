78b4e26853475488e8a1274c21019158379cfcfc
Noi-Git <noi.patsin@gmail.com>
2022-12-07 01:05:17 -0500

    finish style using css from bulma.io

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,11 +1,21 @@
 import React from 'react'
 
-const ProfileCard = ({ title, handle, image }) => {
+const ProfileCard = ({ title, handle, image, description }) => {
   return (
-    <div>
-      <img src={image} alt={`${title} logo`} />
-      <div> Title: {title} </div>
-      <div> Handle: {handle} </div>
+    <div className='card'>
+      <div className='card-image'>
+        <figure className='image is-1by1'>
+          <img src={image} alt={`${title} logo`} />
+        </figure>
+      </div>
+
+      <div className='card-content'>
+        <div className='media-content'>
+          <p className='title is-4'>{title}</p>
+          <p className='subtitle is-6'>{handle}</p>
+        </div>
+        <div className='content'>{description}</div>
+      </div>
     </div>
   )
 }
